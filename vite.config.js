ad83e992f9abdc24d1cd73ed09e8f6901db79fcc
Silvio Gabriel <silviogabrielgsantana@gmail.com>
2024-03-19 16:34:01 -0300

    problema encontrado

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Silvios-bar"
+  // base: "/Silvios-bar" (por algum motivo isso ta dando erro de rota)
 })
